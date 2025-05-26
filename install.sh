da33925a15ec4a76365638c3550507a3ea953436
yosephbernandus <yosephbernandus@gmail.com>
2025-05-26 08:54:29 +0700

    Fix url

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 echo "🚀 Installing Django Project Generator..."
 
 # Download script
-curl -sSL https://raw.githubusercontent.com/user/django-project-generator/master/django_generate.sh -o /tmp/django_generate.sh
+curl -sSL https://raw.githubusercontent.com/yosephbernandus/django-project-generator/master/django_generate.sh -o /tmp/django_generate.sh
 
 # Make executable and install
 chmod +x /tmp/django_generate.sh
